903aab8c2942b29aa4c948cc5f39c254bc0b27e6
Sam Nee <samuel.r.nee@gmail.com>
2020-02-12 00:41:20 -0500

    logo animation works better. layout and contact next

--- src/App.js
+++ src/App.js
@@ -12,18 +12,20 @@ class App extends Component {
   state={
     clicked: false,
     mainLogoClass: "logoPosition1" ,
-    logoImgClass: "logoImg1"
+    logoImgClass: "logoImg1",
+    lightClass: "light"
   }
 
   activateLogos = () => {
     this.state.mainLogoClass === "logoPosition1" ? this.setState({mainLogoClass: "logoPosition2"}) : this.setState({mainLogoClass: "logoPosition1"})
     this.state.logoImgClass === "logoImg1" ? this.setState({logoImgClass: "logoImg2"}) : this.setState({logoImgClass: "logoImg1"})
+    this.state.lightClass === "light" ? this.setState({lightClass: "light-active"}) : this.setState({lightClass: "light"})
   }
 
   render() {
     return (
       <div>
-        <StartButton activateLogos={this.activateLogos} />
+        <StartButton lightClass={this.state.lightClass} activateLogos={this.activateLogos} />
         <Logos imgClass={this.state.logoImgClass} mainClass={this.state.mainLogoClass} />
         <About />
         <Portfolio />
--- src/components/StartButton/StartButton.js
+++ src/components/StartButton/StartButton.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./StartButton.css";
 
-const StartButton = ({ activateLogos }) => {
+const StartButton = ({ lightClass, activateLogos }) => {
 
   return (
     <div class="btnContainer">
       <div className="buttonDiv" onClick={() => activateLogos()}>
         <img className="signature" src="../images/signature.png" alt="sam-nee-signature" />
       </div>
+      <div className={lightClass}></div>
       <div className="insetCircle"></div>
     </div>
   );
